1bc4ea152cf8f5f4a8014c88691990f77911ddc6
Tom Lane <tom@tomlane.me>
2016-01-17 13:07:22 +0000

    Add test for extra details merge

---
+++ NetworkRail.CifParser.Tests/Records/TBasicScheduleRecord.cs
@@ -0,0 +1,36 @@
+﻿using NetworkRail.CifParser.Records;
+using NUnit.Framework;
+
+namespace NetworkRail.CifParser.Tests.Records
+{
+    [TestFixture]
+    public class TBasicScheduleRecord
+    {
+        [TestFixture]
+        class MergeExtraScheduleDetails
+        {
+            [Test]
+            public void returns_expected_result()
+            {
+                var extraScheduleDetails = new BasicScheduleExtraDetailsRecord
+                {
+                    AtsCode = "ATS",
+                    AtocCode = "ATOC",
+                    UicCode = "UIC",
+                    DataSource = "DATASOURCE",
+                    Rsid = "RSID"
+                };
+
+                var basicScheduleRecord = new BasicScheduleRecord();
+
+                basicScheduleRecord.MergeExtraScheduleDetails(extraScheduleDetails);
+
+                Assert.AreEqual(extraScheduleDetails.AtocCode, basicScheduleRecord.AtocCode);
+                Assert.AreEqual(extraScheduleDetails.AtsCode, basicScheduleRecord.AtsCode);
+                Assert.AreEqual(extraScheduleDetails.UicCode, basicScheduleRecord.UicCode);
+                Assert.AreEqual(extraScheduleDetails.DataSource, basicScheduleRecord.DataSource);
+                Assert.AreEqual(extraScheduleDetails.Rsid, basicScheduleRecord.Rsid);
+            }
+        }
+    }
+}
