7b2801e0268d5f5fec3c324505a70cc62bee7dc8
Tom Lane <tom@tomlane.me>
2016-01-20 21:15:42 +0000

    Create interface for new record builders

---
+++ NetworkRail.CifParser/RecordBuilders/ICifRecordBuilder.cs
@@ -0,0 +1,11 @@
+﻿using NetworkRail.CifParser.RecordParsers;
+using NetworkRail.CifParser.Records;
+
+namespace NetworkRail.CifParser.RecordBuilders
+{
+    public interface ICifRecordBuilder<T> where T : ICifRecord
+    {
+        string RecordTypeKey { get; }
+        ICifRecordParser<T> RecordParser { get; } 
+    }
+}
