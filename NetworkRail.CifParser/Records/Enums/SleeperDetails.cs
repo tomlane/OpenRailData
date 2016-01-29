a2b7bbb4b9566bf77576a067017ac76476fbac71
Tom Lane <tom@webdevredemption.com>
2016-01-29 22:23:15 +0000

    Fix SleeperDetails descriptions

--- NetworkRail.CifParser/Records/Enums/SleeperDetails.cs
+++ NetworkRail.CifParser/Records/Enums/SleeperDetails.cs
@@ -7,13 +7,13 @@ namespace NetworkRail.CifParser.Records.Enums
         [Description("Not Available")]
         NotAvailable = 0,
 
-        [Description("First and Standard SleeperDetails")]
+        [Description("First and Standard Sleepers")]
         B = 1,
 
-        [Description("First Class SleeperDetails Only")]
+        [Description("First Class Sleepers Only")]
         F = 2,
 
-        [Description("Standard Class SleeperDetails Only")]
+        [Description("Standard Class Sleepers Only")]
         S = 3
     }
 }
