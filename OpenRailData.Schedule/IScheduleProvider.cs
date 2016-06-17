22598bae1d3462bbf1ae1f837214c784321153c4
Tom Lane <tom@tomlane.me>
2016-06-17 23:30:44 +0100

    Schedule record provider and tiploc suffix migration

---
+++ OpenRailData.Schedule/IScheduleProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using OpenRailData.Domain.ScheduleRecords;
+
+namespace OpenRailData.Schedule
+{
+    public interface IScheduleProvider
+    {
+        Task<ScheduleRecord> GetScheduleRecord(string trainUid, DateTime startDate);
+    }
+}
---
+++ OpenRailData.Schedule/ScheduleProvider.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OpenRailData.Domain.ScheduleRecords;
+using OpenRailData.ScheduleStorage;
+
+namespace OpenRailData.Schedule
+{
+    public class ScheduleProvider : IScheduleProvider
+    {
+        private readonly IScheduleUnitOfWorkFactory _unitOfWorkFactory;
+
+        public ScheduleProvider(IScheduleUnitOfWorkFactory unitOfWorkFactory)
+        {
+            if (unitOfWorkFactory == null)
+                throw new ArgumentNullException(nameof(unitOfWorkFactory));
+
+            _unitOfWorkFactory = unitOfWorkFactory;
+        }
+
+        public async Task<ScheduleRecord> GetScheduleRecord(string trainUid, DateTime startDate)
+        {
+            if (string.IsNullOrWhiteSpace(trainUid))
+                throw new ArgumentException("Value cannot be null or whitespace.", nameof(trainUid));
+
+            IEnumerable<ScheduleRecord> records;
+
+            using (var unitOfWork = _unitOfWorkFactory.Create())
+            {
+                records = await unitOfWork.ScheduleRecords.GetScheduleRecords(trainUid, startDate);
+            }
+
+            return records.OrderByDescending(x => x.StpIndicator).FirstOrDefault();
+        }
+    }
+}
