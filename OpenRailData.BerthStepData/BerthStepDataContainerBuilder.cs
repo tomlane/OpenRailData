7071a5d86e04b7c5ad5c847721fd6faf04acfebf
Tom Lane <tom@tomlane.me>
2016-06-17 16:26:36 +0100

    Add berth step data container builder

---
+++ OpenRailData.BerthStepData/BerthStepDataContainerBuilder.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Practices.Unity;
+
+namespace OpenRailData.BerthStepData
+{
+    public static class BerthStepDataContainerBuilder
+    {
+        public static IUnityContainer Build(IUnityContainer container = null)
+        {
+            if (container == null)
+                container = new UnityContainer();
+
+            container.RegisterType<IBerthStepDataProvider, BerthStepDataProvider>();
+
+            return container;
+        }
+    }
+}
