1ebf0712b752b50b7db9f19a4daa4826fca03079
Tom Lane <tom@tomlane.me>
2016-06-18 14:33:07 +0100

    Implement tiploc provider

---
+++ OpenRailData.Schedule/ITiplocProvider.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using OpenRailData.Domain.ScheduleRecords;
+
+namespace OpenRailData.Schedule
+{
+    public interface ITiplocProvider
+    {
+        Task<TiplocRecord> GetTiplocByCrs(string crs);
+        Task<TiplocRecord> GetTiplocByStanox(string stanox);
+    }
+}
---
+++ OpenRailData.Schedule/TiplocProvider.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OpenRailData.Domain.ScheduleRecords;
+using OpenRailData.ScheduleStorage;
+
+namespace OpenRailData.Schedule
+{
+    public class TiplocProvider : ITiplocProvider
+    {
+        private readonly IScheduleUnitOfWorkFactory _unitOfWorkFactory;
+
+        public TiplocProvider(IScheduleUnitOfWorkFactory unitOfWorkFactory)
+        {
+            if (unitOfWorkFactory == null)
+                throw new ArgumentNullException(nameof(unitOfWorkFactory));
+
+            _unitOfWorkFactory = unitOfWorkFactory;
+        }
+
+        public async Task<TiplocRecord> GetTiplocByCrs(string crs)
+        {
+            if (string.IsNullOrWhiteSpace(crs))
+                throw new ArgumentException("Value cannot be null or whitespace.", nameof(crs));
+
+            List<TiplocRecord> records;
+
+            using (var unitOfWork = _unitOfWorkFactory.Create())
+            {
+                records = await unitOfWork.TiplocRecords.GetTiplocsByCrs(crs);
+            }
+
+            if (records.Count != 1)
+                throw new ArgumentException($"No/Multiple tiploc records found for crs: {crs}.");
+
+            return records.First();
+        }
+
+        public async Task<TiplocRecord> GetTiplocByStanox(string stanox)
+        {
+            if (string.IsNullOrWhiteSpace(stanox))
+                throw new ArgumentException("Value cannot be null or whitespace.", nameof(stanox));
+
+            List<TiplocRecord> records;
+
+            using (var unitOfWork = _unitOfWorkFactory.Create())
+            {
+                records = await unitOfWork.TiplocRecords.GetTiplocsByStanox(stanox);
+            }
+
+            if (records.Count != 1)
+                throw new ArgumentException($"No/Multiple tiploc records found for stanox: {stanox}.");
+
+            return records.First();
+
+        }
+    }
+}
