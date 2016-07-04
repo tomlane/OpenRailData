809664d0f43f787880add5a03694b42fc2d347bc
Tom Lane <tom@tomlane.me>
2016-07-04 22:37:37 +0100

    Implement fetching Darwin schedules from a local file.

---
+++ OpenRailData.Darwin/ScheduleFetching/DarwinLocalFileFetcherOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace OpenRailData.Darwin.ScheduleFetching
+{
+    public class DarwinLocalFileFetcherOptions : IDarwinScheduleFetcherOptions
+    {
+        public string DarwinScheduleFilePath { get; set; }
+
+        public string DarwinScheduleFileSuffix { get; set; }
+    }
+}
---
+++ OpenRailData.Darwin/ScheduleFetching/LocalFileDarwinScheduleFetcher.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Options;
+
+namespace OpenRailData.Darwin.ScheduleFetching
+{
+    public class LocalFileDarwinScheduleFetcher : IDarwinScheduleFetcher
+    {
+        private readonly IOptions<DarwinLocalFileFetcherOptions> _options;
+
+        public LocalFileDarwinScheduleFetcher(IOptions<DarwinLocalFileFetcherOptions> options)
+        {
+            if (options == null)
+                throw new ArgumentNullException(nameof(options));
+
+            _options = options;
+        }
+
+        public byte[] FetchSchedule()
+        {
+            if (string.IsNullOrWhiteSpace(_options.Value.DarwinScheduleFilePath))
+                throw new ArgumentNullException("ScheduleFilePath");
+
+            return File.ReadAllBytes(_options.Value.DarwinScheduleFilePath);
+        }
+    }
+}
